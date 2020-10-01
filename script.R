3025633c723c73be4ad1a172396ba99c5e3d3eca
david mas <david.mas.p@gmail.com>
2020-10-01 19:06:25 +0200

    first commit

---
+++ script.R
@@ -0,0 +1,30 @@
+
+
+
+# funs --------------------------------------------------------------------
+
+vroom_like <- function(paths) {
+  file_list = purrr::map(paths,readr::read_tsv)
+  dplyr::bind_rows(file_list)
+}
+
+# generate files ----------------------------------------------------------
+
+n_files = 10000
+
+df = data.frame(letters,LETTERS)
+
+fs::dir_create("test_data")
+purrr::walk(seq_len(n_files), function(x){
+  readr::write_tsv(df,path = glue::glue("test_data/{x}_test.tsv"))
+})
+
+
+# read files --------------------------------------------------------------
+
+file_names = fs::dir_ls("test_data/")
+
+vroom_like(paths = file_names) -> dat_readr
+ncol(dat_readr)
+vroom::vroom(file = file_names) -> dat_vroom
+ncol(dat_vroom)
